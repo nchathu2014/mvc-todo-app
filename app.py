d94c02ecb9ffbf510073373f29738935a6e36736
T D Nuwan Chathuranga <nchathu2014@gmail.com>
2021-01-14 08:17:48 +0530

    feat: Data persist in Synchronous approach

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -19,6 +19,14 @@ db.create_all()
 @app.route('/')
 def index():
         return render_template('index.html', data = Todo.query.all())
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+        description = request.form.get('description','')
+        todo = Todo(description = description)
+        db.session.add(todo)
+        db.session.commit()
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.debug = True
