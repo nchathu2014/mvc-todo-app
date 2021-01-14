5c179ee5c2250ea09336b48b5d6a83054e9d81d4
T D Nuwan Chathuranga <nchathu2014@gmail.com>
2021-01-14 17:15:01 +0530

    config: add migration scripts using Flask-Migration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request,redirect, url_for,jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['CHEMY_TRACK_MODIFICATIONS'] = False
@@ -7,15 +8,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/tod
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
         __tablename__ = 'todo'
         id = db.Column(db.Integer,primary_key=True)
         description = db.Column(db.String(),nullable=False)
+        completed = db.Column(db.Boolean, nullable=False,default=False)
 
         def __repr__(self):
                 return f'<Todo ID={self.id} DESC={self.description}>'
-
-db.create_all()       
 
 @app.route('/')
 def index():
