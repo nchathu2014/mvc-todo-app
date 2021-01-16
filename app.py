d6175952ce0f0e36839feba10d7238afb945cc9e
T D Nuwan Chathuranga <nchathu2014@gmail.com>
2021-01-16 07:47:57 +0530

    migration: complete joining the tables todolists and todos

--- app.py
+++ app.py
@@ -9,14 +9,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+#child table
 class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
   completed = db.Column(db.Boolean, nullable=False)
+  list_id = db.Column(db.Integer,db.ForeignKey('todolists.id'),nullable=False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
+
+#parent table
+class TodoList(db.Model):
+  __tablename__ = 'todolists'
+  id=db.Column(db.Integer,primary_key=True)
+  name=db.Column(db.String(),nullable=False)
+  todos = db.relationship('Todo',backref='list',lazy=True)
+
 
 @app.route('/todos/<todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
